3048054df496deacd98895521f99bd73cb58158b
andy-tai <atai@atai.org>
2016-01-26 00:48:29 -0800

    remove the earlier change to shader path handling; adapt in app code instead

--- Source/Urho3D/Graphics/OpenGL/OGLGraphics.cpp
+++ Source/Urho3D/Graphics/OpenGL/OGLGraphics.cpp
@@ -2068,14 +2068,11 @@ ShaderVariation* Graphics::GetShader(ShaderType type, const char* name, const ch
     {
         ResourceCache* cache = GetSubsystem<ResourceCache>();
 
-        String fullShaderName = name ;
-        if (lastShaderName_ == name && !cache->Exists(name))
-{
-        fullShaderName = shaderPath_ + name + shaderExtension_;
+        String fullShaderName = shaderPath_ + name + shaderExtension_;
         // Try to reduce repeated error log prints because of missing shaders
         if (lastShaderName_ == name && !cache->Exists(fullShaderName))
             return 0;
-}
+
         lastShader_ = cache->GetResource<Shader>(fullShaderName);
         lastShaderName_ = name;
     }
